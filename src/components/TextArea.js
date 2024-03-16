b7f02d3d0d4e2fc3fc9bb2da1cc8dfb6ddbf69e9
dakshy <daksh.yadav.2704@gmail.com>
2024-03-16 21:47:34 +0100

    Beautify JSON, Alerts

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -9,6 +9,7 @@ function getButton(title, color, disabled, workfn) {
 export default function TextArea() {
 
     const [text, setText] = useState("");
+    const [alertText, setAlert] = useState(null);
 
     const handleChange = (event)=>{
         setText(event.target.value)
@@ -20,12 +21,27 @@ export default function TextArea() {
     const setLowerCase = ()=>{
         setText(text.toLowerCase());
     }
+    const beautifyJSON = ()=>{
+        try {
+            let jsObj = JSON.parse(text);
+            setText(JSON.stringify(jsObj, null, 4));
+        } catch (e) {
+            setAlert({text:"Sorry, but the input cannot be parsed into a JSON object.",color:"danger"});
+            setTimeout(()=>{
+                setAlert(null);
+            }, 2000);
+        }
+    }
     const copyText = ()=>{
         navigator.clipboard.writeText(text);
     }
 
     return (
         <>
+            {alertText && <div className={`alert alert-dismissible alert-${alertText.color}`}>
+                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                {alertText.text}
+            </div>}
             <div>
             <label htmlFor="mainText" className="form-label mt-4">Text Converter</label>
             <textarea 
@@ -38,7 +54,7 @@ export default function TextArea() {
             </div>
             {getButton("Lower Case", "primary", (text.length===0), setLowerCase)}
             {getButton("Upper Case", "secondary", (text.length===0), setUpperCase)}
-            {getButton("Soon!", "success", true)}
+            {getButton("Beautify JSON", "success", (text.length===0), beautifyJSON)}
             {getButton("Soon!", "info", true)}
             {getButton("Soon!", "warning", true)}
             {getButton("Soon!", "danger", true)}
